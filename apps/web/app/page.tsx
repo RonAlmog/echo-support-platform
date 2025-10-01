b78b5a7494938a5fa86397b1a0d29225bec2cd02
Ron Almog <ron8132@gmail.com>
2025-10-01 09:58:20 -0400

    access convex backend from web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,15 @@
 import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+import { useQuery } from "convex/react";
+import { api } from "../../../packages/backend/convex/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getAllUsers) || [];
+  console.log(users);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Apps/Web</h1>
         <Button size="sm">Button</Button>
         <p>{`2 + 3 = ${add(2, 3)}`}</p>
       </div>
