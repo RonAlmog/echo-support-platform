9ed4d51a4f62b84adba97eda73070d68d3cee243
Ron Almog <ron8132@gmail.com>
2025-10-04 08:50:22 -0400

    add signin, signup pages

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -7,20 +7,15 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error("NEXT_PUBLIC_CONVEX_URL is not defined");
+  }
   const convex = new ConvexReactClient(
     process.env.NEXT_PUBLIC_CONVEX_URL || ""
   );
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        {children}
-      </ConvexProviderWithClerk>
-    </NextThemesProvider>
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
   );
 }
