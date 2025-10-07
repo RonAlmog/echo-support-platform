3ea1aa05367d286e87e03766edc04bb746098df7
Ron Almog <ron8132@gmail.com>
2025-10-07 18:53:58 -0400

    add vapi-ai package

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
 import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
-import { AuthLayout } from "@/modules/auth/ui/layouts/auth-layout";
 
 export default function DashLayout({
   children,
